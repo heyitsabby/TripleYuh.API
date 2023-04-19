3a247d4e9b38f2126e8b6507670b0d3c34222339
Abby <55665984+heyitsabby@users.noreply.github.com>
2023-04-19 05:26:41 -0700

    Handle generating refresh token

--- Infrastructure/Authorization/JwtUtils.cs
+++ Infrastructure/Authorization/JwtUtils.cs
@@ -2,12 +2,14 @@
 using Application.Helpers;
 using Domain.Entities;
 using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -45,12 +47,57 @@ namespace Infrastructure.Authorization
 
         public RefreshToken GenerateRefreshToken(string ipAddress)
         {
-            throw new NotImplementedException();
+            var refreshToken = new RefreshToken
+            {
+                // token is a cryptographically strong random sequence of values
+                Token = Convert.ToHexString(RandomNumberGenerator.GetBytes(64)),
+                // token is valid for 7 days
+                Expires = DateTime.UtcNow.AddDays(7),
+                Created = DateTime.UtcNow,
+                CreatedByIp = ipAddress
+            };
+
+            // ensure token is unique by checking against db
+            var tokenIsUnique = !context.Accounts.Any(
+                account => account.RefreshTokens.Any(token => token.Token == refreshToken.Token));
+
+            if (!tokenIsUnique)
+                return GenerateRefreshToken(ipAddress);
+
+            return refreshToken;
         }
 
         public int? ValidateJwtToken(string token)
         {
-            throw new NotImplementedException();
+            if (token == null) { return null; }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(appsettings.Secret);
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters 
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+
+                var accountId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                // return account id from JWT token if validation successful
+                return accountId;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
