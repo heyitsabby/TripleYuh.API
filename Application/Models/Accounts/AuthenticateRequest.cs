95d808f38b5759bc84d020296d8060a2d4f5a3e0
Abby <55665984+heyitsabby@users.noreply.github.com>
2023-04-26 08:55:31 -0700

    Remove uneeded models

--- Application/Models/Accounts/AuthenticateRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Models.Accounts
-{
-    public class AuthenticateRequest
-    {
-        public string Email { get; set; } = string.Empty;
-
-        public string Password { get; set; } = string.Empty;
-    }
-}
