1bb13bfad3401ee0013e1d774f1318a3629b5868
Abby <55665984+heyitsabby@users.noreply.github.com>
2023-04-27 06:44:31 -0700

    Handle getting all posts

---
+++ Application/Features/Posts/Queries/GetAllPostsQuery/GetAllPostsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Models.Accounts;
+using MediatR;
+
+namespace Application.Features.Posts.Queries.GetAllPostsQuery
+{
+    public class GetAllPostsQuery : IRequest<IEnumerable<PostResponse>>
+    {
+    }
+}
