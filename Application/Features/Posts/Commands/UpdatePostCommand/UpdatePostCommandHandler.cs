5d74cf3e940ae6cccc6c2f9fc9269929fc8076e2
Abby <55665984+heyitsabby@users.noreply.github.com>
2023-04-26 22:30:43 -0700

    Handle updating posts

---
+++ Application/Features/Posts/Commands/UpdatePostCommand/UpdatePostCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Interfaces;
+using Application.Models.Accounts;
+using MediatR;
+
+namespace Application.Features.Posts.Commands.UpdatePostCommand
+{
+    public class UpdatePostCommandHandler : IRequestHandler<UpdatePostCommand, PostResponse>
+    {
+        private readonly IPostService postService;
+
+        public UpdatePostCommandHandler(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+        public async Task<PostResponse> Handle(UpdatePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = await postService.UpdatePostAsync(request.Id, request.Username, request.Body);
+
+            return post;
+        }
+    }
+}
--- WebApi/Controllers/PostsController.cs
+++ WebApi/Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Common.Security;
 using Application.Features.Posts.Commands.CreateLinkPostCommand;
 using Application.Features.Posts.Commands.CreateTextPostCommand;
+using Application.Features.Posts.Commands.UpdatePostCommand;
 using Application.Models.Accounts;
 using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,24 @@ namespace WebApi.Controllers
         [HttpPost("text")]
         public async Task<ActionResult<PostResponse>> CreateTextPostAsync(CreateTextPostCommand command)
         {
+            if (command.Username != Account?.Username && Account?.Role != Role.Admin)
+            {
+                return Unauthorized(new { message = "Unauthorized" });
+            }
+
+            var post = await Mediator.Send(command);
+
+            return Ok(post);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<PostResponse>> UpdateAsync(int id, UpdatePostCommand command)
+        {
+            if (command.Id != id)
+            {
+                return BadRequest(new { message = "Ids don't match." });
+            }
+
             if (command.Username != Account?.Username && Account?.Role != Role.Admin)
             {
                 return Unauthorized(new { message = "Unauthorized" });
